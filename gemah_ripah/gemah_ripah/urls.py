b2c28658c77b67cd84da59ae962787ed52b2f16c
rpribadi <pribadi.riki@gmail.com>
2014-11-19 17:15:17 +0800

    Fix login page. Set products as default landing page.

--- gemah_ripah/gemah_ripah/urls.py
+++ gemah_ripah/gemah_ripah/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(pattern_name='products:index')),
+
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name="login"),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
 
@@ -9,5 +12,5 @@ urlpatterns = patterns('',
     url(r'^sales/', include('sales.urls', namespace="sales")),
     url(r'^purchase/', include('purchase.urls', namespace="purchase")),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^secured/', include(admin.site.urls)),
 )
