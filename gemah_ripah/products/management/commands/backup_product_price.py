9880c79f5ec4e2593a4c33a6abee1f96a0e863d3
rpribadi <pribadi.riki@gmail.com>
2014-11-28 23:53:22 +0800

    Add backup product price command.

---
+++ gemah_ripah/products/management/commands/backup_product_price.py
@@ -0,0 +1,57 @@
+import csv
+import datetime
+import glob
+import os
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import send_mail, EmailMessage
+
+from products.models import Product
+
+
+class Command(BaseCommand):
+    help = 'Send backup product price'
+
+    def handle(self, *args, **options):
+        try:
+            self.process()
+        except Exception, e:
+            raise CommandError('Error sending backup product price. Reason: %s' % (e, ))
+
+    def process(self):
+        now = datetime.datetime.now()
+        subject = "Product Price Per %s" % now
+        sender = "shop@gemahripah.net"
+        recipients = ["pribadi.riki@gmail.com", "ulnita.prihastie@gmail.com"]
+        message = "Please find the attachment for the latest product price.\n\nRegards,\nGemah Ripah"
+        attachment = "product-%s.csv" % now.strftime("%d-%b-%Y")
+
+        import csv
+        with open(attachment, 'wb') as f:
+            writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+
+            writer.writerow(['PRODUCT', 'BUY PRICE', 'SELL PRICE', 'MARGIN', 'STOCK'])
+
+            for product in Product.objects.filter(is_active=True):
+                if product.purchaseitem_set.all().count():
+                    product.buy_price = product.purchaseitem_set.all()[0].price
+                    product.margin = product.price - product.buy_price
+                else:
+                    product.buy_price = "-"
+                    product.margin = "-"
+                writer.writerow([product.name, product.buy_price, product.price, product.margin, product.stock])
+
+        email = EmailMessage(
+            subject,
+            message,
+            sender,
+            recipients,
+            [],
+            headers={'Reply-To': 'pribadi.riki@gmail.com'}
+        )
+
+        with open(attachment) as f:
+            email.attach(attachment, f.read(), 'text/plain')
+
+        email.send()
