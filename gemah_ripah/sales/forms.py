9a56c819a62eda398c72e67ed781b6e94ab52afe
rpribadi <pribadi.riki@gmail.com>
2015-01-24 12:24:35 +0800

    Add inactive product info in product selection in sales.

--- gemah_ripah/sales/forms.py
+++ gemah_ripah/sales/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 
+from products.models import Product
+
 from models import Sales, SalesItem
 
 
@@ -14,3 +16,14 @@ class SalesItemForm(ModelForm):
     class Meta:
         model = SalesItem
         exclude = ()
+
+
+    def __init__(self, *args, **kwargs):
+        super(SalesItemForm, self).__init__(*args, **kwargs)
+
+        # get different list of choices here
+        choices = [(None, "----------")]
+        for product in Product.objects.values_list('id', 'name', 'is_active'):
+            name = product[1] if product[2] else "(inactive) - %s" % product[1]
+            choices.append((product[0], name))
+        self.fields["product"].choices = choices
