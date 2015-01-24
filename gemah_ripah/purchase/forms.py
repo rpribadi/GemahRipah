d5fc0e46ab111eee31e6dc2839d2f8238b574426
rpribadi <pribadi.riki@gmail.com>
2015-01-24 12:26:53 +0800

    Add product status info in product choices in purchase.

--- gemah_ripah/purchase/forms.py
+++ gemah_ripah/purchase/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 
+from products.models import Product
+
 from models import Purchase, PurchaseItem
 
 
@@ -14,3 +16,13 @@ class PurchaseItemForm(ModelForm):
     class Meta:
         model = PurchaseItem
         exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super(PurchaseItemForm, self).__init__(*args, **kwargs)
+
+        # get different list of choices here
+        choices = [(None, "----------")]
+        for product in Product.objects.values_list('id', 'name', 'is_active'):
+            name = product[1] if product[2] else "(inactive) - %s" % product[1]
+            choices.append((product[0], name))
+        self.fields["product"].choices = choices
